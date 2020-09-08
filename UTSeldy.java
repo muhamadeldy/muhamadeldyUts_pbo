bfefd7b07b7f0527a67cf756e4b3814ac6c54d01
muhamadeldy <70957263+muhamadeldy@users.noreply.github.com>
2020-09-08 21:01:59 +0700

    Add files via upload

---
+++ UTSeldy.java
@@ -0,0 +1,13 @@
+package UTS;
+
+public class UTSeldy {
+    public static void main(String[] args) {
+        String alfabet = "ABCDEFGHIJKLMNOPQRSTUVWQYZ";
+
+        System.out.println(alfabet);
+        System.out.println("" + alfabet.substring(4,5));
+        System.out.println("" + alfabet.substring(11,12));
+        System.out.println("" + alfabet.substring(3,4));
+        System.out.println("" + alfabet.substring(24,25));
+    }
+}
